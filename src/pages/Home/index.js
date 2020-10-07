6bff5119d46b58c32616c914f9cea324f067d478
JosephMartins <neto-neon@live.com>
2020-10-07 10:32:25 -0300

    feature: listagem

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Modal from '../../components/Modal';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 const Home = () => {
   const [modal, setModal] = useState(null);
-  console.log(modal)
-  return (
+  const [escolas, setEscolas] = useState([]);
+
+  useEffect(() =>  {
+    api.get('/escola').then(response => setEscolas(response.data));
+
+  }, []);
+
+
+   return (
     <>
     <div className="container">
 
@@ -16,13 +25,14 @@ const Home = () => {
       <button onClick={() => setModal(true)}>Adicionar</button>
       <Modal modal={modal} setModal={setModal}></Modal>
       </div>
-
-      <section className="box__card">
+      
+      {escolas.map(({nome, ensino, cep, id}) => (
+      <section className="box__card" key={id}>
         <article className="card">
           <div className="card__info"> 
-            <h3>Centro de ensino médio 03</h3>
-            <p>Tipo :  <span>Fundamental</span></p>
-            <p>Cep: <span>72215063</span></p>
+            <h3>{nome}</h3>
+            <p>Tipo :  <span>{ensino}</span></p>
+            <p>Cep: <span>{cep}</span></p>
           </div>
           <div className="card__buttons">
             <button style={{background: '#b52e2e'}}>Excluir</button>
@@ -32,49 +42,7 @@ const Home = () => {
           </div>
         </article>
       </section>
-
-      <section className="box__card">
-        <article className="card">
-          <div className="card__info"> 
-            <h3>Centro de ensino médio 03</h3>
-            <p>Tipo :  <span>Fundamental</span></p>
-            <p>Cep: <span>72215063</span></p>
-          </div>
-          <div className="card__buttons">
-            <button style={{background: '#b52e2e'}}>Excluir</button>
-            <button className="button__red">Alterar</button>
-            <div className="card__link">
-            </div>
-          </div>
-        </article>
-      </section><section className="box__card">
-        <article className="card">
-          <div className="card__info"> 
-            <h3>Centro de ensino médio 03</h3>
-            <p>Tipo :  <span>Fundamental</span></p>
-            <p>Cep: <span>72215063</span></p>
-          </div>
-          <div className="card__buttons">
-            <button style={{background: '#b52e2e'}}>Excluir</button>
-            <button className="button__red">Alterar</button>
-            
-          </div>
-        </article>
-      </section><section className="box__card">
-        <article className="card">
-          <div className="card__info"> 
-            <h3>Centro de ensino médio 03</h3>
-            <p>Tipo :  <span>Fundamental</span></p>
-            <p>Cep: <span>72215063</span></p>
-          </div>
-          <div className="card__buttons">
-            <button style={{background: '#b52e2e'}}>Excluir</button>
-            <button className="button__red">Alterar</button>
-            <div className="card__link">
-            </div>
-          </div>
-        </article>
-      </section>
+      ))}
       
     </div>
     </>
